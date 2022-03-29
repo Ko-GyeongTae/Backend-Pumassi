23963b8f52dae85f42e137c89848121fdf39718d
Ko-GyeongTae <kokt0203@naver.com>
2022-03-29 16:05:16 +0900

    feat(setting): typeorm

---
+++ src/shared/entities/auth.entity.ts
@@ -0,0 +1,52 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Room } from './room.entity';
+
+@Entity()
+export class Auth {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 20 })
+  email: string;
+
+  @Column({ type: 'varchar', length: 20 })
+  name: string;
+
+  @Column({ type: 'varchar', length: 50 })
+  password: string;
+
+  @Column({ type: 'integer' })
+  entranceYear: number;
+
+  @Column({ type: 'integer' })
+  grade: number;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+  })
+  updatedAt: Date;
+
+  @DeleteDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+  })
+  deletedAt: Date;
+
+  @OneToMany(() => Room, (room) => room.member)
+  rooms: Room[];
+}
