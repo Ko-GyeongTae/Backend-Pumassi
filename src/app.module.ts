3bacb916520cc9180f81afc2563f00ebd572beeb
Ko-GyeongTae <kokt0203@naver.com>
2022-03-29 15:10:37 +0900

    feat(ejs): create front ejs engine for chat test

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ChatModule } from './chat/chat.module';
+import { EjsModule } from './ejs/ejs.module';
 
 @Module({
-  imports: [AuthModule, ChatModule],
+  imports: [AuthModule, ChatModule, EjsModule],
   controllers: [AppController],
   providers: [AppService],
 })
