d835b240f18ff78d209a1ed73f13d82683d7e7a8
Ko-GyeongTae <kokt0203@naver.com>
2022-04-18 14:14:47 +0900

    feat(auth): create sign in/up api function

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express';
+import { signIn, signUp } from '../api/auth';
 
 const router = Router();
 
-router.post('/login');
+router.post('/sign-in', signIn);
+router.post('/sign-up', signUp);
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,17 @@
 import express, { Application } from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import routes from './routes';
+import bodyParser from 'body-parser';
 
 const app: Application = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', routes);
 
 app.listen(4120, () => {
   console.log(`
