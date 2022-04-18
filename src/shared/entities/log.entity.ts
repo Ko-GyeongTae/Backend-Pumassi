3d5841d0ee9ec75bd0e2493f68a39804fdf040e3
Ko-GyeongTae <kokt0203@naver.com>
2022-04-18 21:41:41 +0900

    feat(log): create save log logic

---
+++ src/shared/entities/log.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Log {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'char', length: '20' })
+  ip: string;
+
+  @Column({ type: 'varchar', length: '40' })
+  agent: string;
+
+  @Column({ type: 'varchar', length: '10' })
+  method: string;
+
+  @Column({ type: 'varchar', length: '40' })
+  originalUrl: string;
+}
