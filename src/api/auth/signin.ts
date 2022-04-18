17d275221c83dff838c4780cc033b12fdbe17bf6
Ko-GyeongTae <kokt0203@naver.com>
2022-04-18 21:41:02 +0900

    feat(tmp): save api

--- src/api/auth/signin.ts
+++ src/api/auth/signin.ts
@@ -1,6 +1,21 @@
+import { validateOrReject } from 'class-validator';
 import { Request, Response } from 'express';
+import { SignInRequest } from '../../shared/dto/auth.dto';
 
-export const signIn = (req: Request, res: Response) => {
-  console.log(req.body);
-  res.status(200).json({ message: 'success' });
+export const signIn = async (req: Request, res: Response): Promise<void> => {
+  const { body } = req;
+
+  const data = new SignInRequest();
+  data.email = body.email;
+  data.password = body.password;
+
+  validateOrReject(data)
+    .then((r) => {
+      console.log(r);
+      res.status(200).json({ message: 'success' });
+    })
+    .catch((e) => {
+      console.log(e);
+      res.status(400).json({ message: '입력 형식이 올바르지 않습니다.' });
+    });
 };
--- src/api/auth/signup.ts
+++ src/api/auth/signup.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from 'express';
 
-export const signUp = (req: Request, res: Response) => {
-  console.log(req.body);
-  res.status(200).json({ message: 'success' });
+export const signUp = async (req: Request, res: Response) => {
+  const { body } = req;
+  const { ip, method, originalUrl } = req;
+  console.log(req.headers['user-agent']);
+  console.log(body);
+  res.sendStatus(201);
 };
