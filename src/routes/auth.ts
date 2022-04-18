9b0a9d2728b6cfd195405ab3d9aee8a1278d9534
Ko-GyeongTae <kokt0203@naver.com>
2022-04-18 16:23:08 +0900

    feat(validate): create validate middleware

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,19 @@
 import { Router } from 'express';
 import { signIn, signUp } from '../api/auth';
+import { SignInRequest, SignUpRequest } from '../shared/dto/auth.dto';
+import { validateObj } from '../utils/validateObj';
 
 const router = Router();
 
-router.post('/sign-in', signIn);
-router.post('/sign-up', signUp);
+router.post(
+  '/sign-in',
+  validateObj(SignInRequest, '로그인 정보 입력 형식이 잘못되었습니다.'),
+  signIn,
+);
+router.post(
+  '/sign-up',
+  validateObj(SignUpRequest, '회원가입 정보 입력 형식이 잘못되었습니다.'),
+  signUp,
+);
 
 export default router;
