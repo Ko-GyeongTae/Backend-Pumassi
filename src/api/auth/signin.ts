00b942b01d3fea3b8bef28d392fe0766fbcd1a6a
Ko-GyeongTae <kokt0203@naver.com>
2022-04-29 09:26:45 +0900

    feat(sign-in): modify api

--- src/api/auth/signin.ts
+++ src/api/auth/signin.ts
@@ -2,43 +2,14 @@ import { Request, Response } from 'express';
 import crypto from 'crypto';
 import jwt from 'jsonwebtoken';
 import { User } from '../../shared/entities/user.entity';
-import { getConnection, getRepository } from 'typeorm';
-import { Room } from '../../shared/entities/room.entity';
+import { getRepository } from 'typeorm';
 
 export const signIn = async (req: Request, res: Response): Promise<void> => {
   const { email, password } = req.body;
 
   const userRepository = getRepository(User);
-  const queryRunner = getConnection().createQueryRunner();
-  const connection = getConnection();
-  await queryRunner.connect();
 
-  const temp = await queryRunner.manager.query('SELECT * FROM user');
-  console.log(temp);
-  const _temp = await queryRunner.manager.find(User);
-  console.log(_temp);
-  const __temp = await userRepository.find();
-  console.log(__temp);
-  const ___temp = await getRepository(User)
-    .createQueryBuilder('user')
-    .getMany();
-  console.log(___temp);
-
-  const ____temp = await getRepository(User)
-    .createQueryBuilder('user')
-    .getRawMany();
-
-  console.log(____temp);
-  //const user = await userRepository.findOne({ where: { email } });
-
-  // const queryRunner = getConnection().createQueryRunner();
-  // await queryRunner.connect();
-
-  const userList = await queryRunner.manager.query(
-    `SELECT * FROM user WHERE email = '${email}' LIMIT 1`,
-  );
-
-  const user: User = userList[0];
+  const user = await userRepository.findOne({ where: { email } });
 
   if (!user) {
     res.status(401).json({
