4c5130b60ae843d7b4af23734f9712ae4ce640d0
Ko-GyeongTae <kokt0203@naver.com>
2022-05-08 22:47:00 +0900

    feat(route): post route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,18 +1,21 @@
 import { Router } from 'express';
 import { signIn, signUp } from '../api/auth';
 import { SignInRequest, SignUpRequest } from '../shared/dto/auth.dto';
-import { validateMiddleware } from '../middleware/validateMiddleware';
+import { validateBodyMiddleware } from '../middleware/validateMiddleware';
 
 const router = Router();
 
 router.post(
   '/sign-in',
-  validateMiddleware(SignInRequest, '로그인 정보 입력 형식이 잘못되었습니다.'),
+  validateBodyMiddleware(
+    SignInRequest,
+    '로그인 정보 입력 형식이 잘못되었습니다.',
+  ),
   signIn,
 );
 router.post(
   '/sign-up',
-  validateMiddleware(
+  validateBodyMiddleware(
     SignUpRequest,
     '회원가입 정보 입력 형식이 잘못되었습니다.',
   ),
