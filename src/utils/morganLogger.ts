857b4ed5a5f283112b462f69d3cab927b5c7c8db
Ko-GyeongTae <kokt0203@naver.com>
2022-05-10 09:46:24 +0900

    feat(logger): create mkdir (if folder does not exist)

--- src/utils/morganLogger.ts
+++ src/utils/morganLogger.ts
@@ -2,7 +2,11 @@ import fs from 'fs';
 import path from 'path';
 import { todayString } from './date';
 
+const dirPath = path.join(__dirname, `../../logs/`);
 export const createStream = (): fs.WriteStream => {
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath);
+  }
   const accessLogStream = fs.createWriteStream(
     path.join(__dirname, `../../logs/${todayString()}.log`),
     {
