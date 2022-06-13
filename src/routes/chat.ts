ea880173e6afc2dd8b9cb02cfb058fff556fba52
Ko-GyeongTae <kokt0203@naver.com>
2022-06-13 15:28:05 +0900

    Feat: createRoom api

--- src/routes/chat.ts
+++ src/routes/chat.ts
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 import { jwtMiddleware } from '../middleware/jwtMiddleware';
-import { getRoomList } from '../api/chat';
+import { createRoom, getRoomList } from '../api/chat';
+import { CreateRoomDto } from '../shared/dto/chat.dto';
+import { validateBodyMiddleware } from '../middleware/validateMiddleware';
 
 const router = Router();
 
-router.get('/list', jwtMiddleware, getRoomList);
+router.use(jwtMiddleware);
+router.post(
+  '/',
+  validateBodyMiddleware(CreateRoomDto, '채팅방 생성 형식이 잘못되었습니다.'),
+  createRoom,
+);
+router.get('/list', getRoomList);
 
 export default router;
